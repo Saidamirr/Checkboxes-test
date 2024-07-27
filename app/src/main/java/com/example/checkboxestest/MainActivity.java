5c401f4bdf390030c3d5ba1065ae20cb9a45cba2
saidamir-home <saidamirxonn@gmail.com>
2024-07-27 14:24:04 +0500

    first push

---
+++ app/src/main/java/com/example/checkboxestest/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.checkboxestest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RadioGroup rgMeat = (RadioGroup) findViewById(R.id.radioGroup2);
+
+
+        Button btnOrder = findViewById(R.id.btnOrder);
+        TextView orderCheck = findViewById(R.id.orderCheck);
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CheckBox salty = findViewById(R.id.cbSalty);
+                CheckBox tomato = findViewById(R.id.cbTomato);
+                CheckBox fresh = findViewById(R.id.cbFresh);
+                CheckBox milk = findViewById(R.id.cbMilkSalad);
+                String resultOrder = "You ordered osh palov with " +
+                        ((RadioButton)findViewById(rgMeat.getCheckedRadioButtonId())).getText();
+                if (salty.isChecked()) {resultOrder += ", salty salad";}
+                if (tomato.isChecked()) {resultOrder += ", tomato salad";}
+                if (fresh.isChecked()) {resultOrder += ", fresh salad";}
+                if (milk.isChecked()) {resultOrder += ", milky salad";}
+
+                orderCheck.setText(resultOrder );
+            }
+        });
+    }
+}
